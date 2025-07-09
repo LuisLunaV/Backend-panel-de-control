064ab52d8d5552fce9e5e3312c296387923cd841
LuisLunaV <luis.a.luna.v@outlook.com>
2025-07-08 21:07:28 -0600

    serve

--- src/model/server.ts
+++ src/model/server.ts
@@ -29,7 +29,7 @@ export class Server{
     this.app.use(express.json());
     this.app.use(cookieParser());
     this.app.use(cors({
-        origin: [CLIENT_BASE_URL as string, 'http://localhost:5173'], 
+        origin: [CLIENT_BASE_URL as string || 'http://localhost:5173'], 
         methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
         credentials: true, 
         optionsSuccessStatus: 200, 

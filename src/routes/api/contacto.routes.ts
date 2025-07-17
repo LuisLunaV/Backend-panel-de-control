ee6666b2057f0df8839430811f12e40ff1216bd2
LuisLunaV <luis.a.luna.v@outlook.com>
2025-07-16 23:33:19 -0600

    Agregamos rutas obtener mensaje y eliminar

--- src/routes/api/contacto.routes.ts
+++ src/routes/api/contacto.routes.ts
@@ -3,19 +3,34 @@ import { ContactoController } from '../../controller/contacto.controller';
 import { validateJWT } from '../../middlewares/validate-jwt';
 import { validateProperties } from '../../middlewares/validate-properties';
 import { validarMensaje } from '../../helpers/validadores-generales';
-import { check } from 'express-validator';
+import { check, param } from 'express-validator';
 import { ContactoServices } from '../../services/contacto/contacto.service';
 
 const router = Router();
 const contactoServicio = new ContactoServices();
 const contactoController = new ContactoController( contactoServicio );
 
-router.get('/api/v1/bandeja/all-messages', contactoController.getMessages)
+router.get('/api/v1/bandeja/all-messages',[
+    validateJWT,
+    validateProperties
+],contactoController.getMessages);
+
+router.get('/api/v1/bandeja/contenido-msg/:id',
+    param('id').isInt(),[
+    validateJWT,
+    validateProperties
+],contactoController.getMessageId);
 
 router.post('/api/v1/form-messages/send', [
     check('Msg_email', 'El email no es valido').isEmail(),
     check('Msg_texto', 'EL mensaje no debe estar vacio').notEmpty().custom( validarMensaje ),
     validateProperties
 ], contactoController.addMessage);
+
+router.delete('/api/v1/bandeja/delete-msg/:id',
+param('id').isInt(),[
+    validateJWT,
+    validateProperties
+],contactoController.deleteMessage );
 
 export default router;

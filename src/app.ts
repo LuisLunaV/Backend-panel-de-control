bb8e995b00c65b2cf2e3d70d82abc5bd5177ffc4
LuisLunaV <luis.a.luna.v@outlook.com>
2026-01-20 00:20:43 -0600

    pruebas de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { Server } from './model/server';
-import { PORT } from './config/envs';
+import { MYSQLPORT } from './config/envs';
 (()=>{
     main();
 })()
 
  async function main(){
     
-    new Server( Number(PORT) ).start();
+    new Server( Number(MYSQLPORT) ).start();
  }
